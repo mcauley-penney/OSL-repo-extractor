61c5a812d521c91db550e67332bf95ff88eef701
McAuley Penney <jacobmpenney@gmail.com>
2023-01-15 12:17:15 -0700

    refactor(utils):
    - make _merge_dicts_recursive recursive call
      private, to match function name
    
    - make write_dict_to_jsonfile private and move
      it so it is under the method calls that use it
      in the proper order
    
    - remove the methods that compose
      read_jsonfile_into_dict() and simply execute
      their functionality in that function. They
      weren't used by other functions as helpers and
      were a couple of lines long

--- repo_extractor/utils.py
+++ repo_extractor/utils.py
@@ -87,13 +87,34 @@ def _merge_dicts_recursive(base_dict: dict, add_dict: dict) -> None:
             and isinstance(base_dict[key], dict)
             and isinstance(add_dict[key], dict)
         ):
-            # recurse
-            merge_dicts_recursive(base_dict[key], add_dict[key])
+            _merge_dicts_recursive(base_dict[key], add_dict[key])
 
         else:
             # assign the new value from the last round of calls to the existing
             # key
             base_dict[key] = add_dict[key]
+
+
+def _write_dict_to_jsonfile(out_dict: dict, out_path: str) -> None:
+    """
+    Ensure output file exists and write Python dictionary to it as JSON.
+
+    Args:
+        out_dict (dict): dictionary to write as JSON.
+        out_path (str): path to write output to.
+
+    Raises:
+        FileNotFoundError: no file found at given path.
+    """
+    mk_json_outpath(out_path)
+
+    try:
+        with open(out_path, "w", encoding="UTF-8") as json_outfile:
+            json.dump(out_dict, json_outfile, ensure_ascii=False, indent=2)
+
+    except FileNotFoundError:
+        print(f"\nFile at {out_path} not found!")
+        sys.exit(1)
 
 
 def mk_json_outpath(out_path: str):
@@ -106,8 +127,6 @@ def mk_json_outpath(out_path: str):
 
     Args:
         out_dir (str): dir to init output file in.
-        repo_title (str): the name of the repo, e.g. "bar" in "foo/bar".
-        output_type (str): type of output being created.
 
     Raises:
         FileExistsError: if the file that we are attempting to
@@ -131,33 +150,6 @@ def mk_json_outpath(out_path: str):
 
     except FileExistsError:
         pass
-
-
-def _read_json_into_text(in_path: str) -> str:
-    """
-    Read file contents into string.
-
-    Used for reading JSON data out of JSON files.
-
-    Args:
-        in_path (str): path to file to read contents from.
-
-    Raises:
-        FileNotFoundError: hard exit if a file cannot be found.
-
-    Returns:
-        str: text from file.
-    """
-    try:
-        with open(in_path, "r", encoding="UTF-8") as file_obj:
-            json_text = file_obj.read()
-
-    except FileNotFoundError:
-        print(f'\nFile at "{in_path}" not found!')
-        sys.exit(1)
-
-    else:
-        return json_text
 
 
 def read_file_line(in_path: str) -> str:
@@ -185,91 +177,3 @@ def read_file_line(in_path: str) -> str:
 
     else:
         return file_text.strip().strip("\n")
-
-
-def read_jsonfile_into_dict(in_path: str) -> dict:
-    """
-    Read the contents of the provided JSON file into a dictionary.
-
-    Args:
-        in_path (str): path to JSON file to read from.
-
-    Returns:
-        dict: dictionary constructed from JSON contents.
-    """
-    json_text = _read_json_into_text(in_path)
-
-    json_dict = read_jsontext_into_dict(json_text)
-
-    return json_dict
-
-
-def read_jsontext_into_dict(json_text: str) -> dict:
-    """
-    Convert text from JSON file into a python dict.
-
-    Args:
-        json_text (str): text from JSON file.
-
-    Raises:
-        JSONDecodeError: if no valid JSON content exists, ignore.
-            In this context, it simply means that we are writing
-            JSON to a new file.
-
-    Returns:
-        dict: dictionary constructed from JSON contents.
-    """
-    try:
-        json_dict = json.loads(json_text)
-
-    except JSONDecodeError:
-        return {}
-
-    else:
-        return json_dict
-
-
-def write_dict_to_jsonfile(out_dict: dict, out_path: str) -> None:
-    """
-    Ensure output file exists and write Python dictionary to it as JSON.
-
-    Args:
-        out_dict (dict): dictionary to write as JSON.
-        out_path (str): path to write output to.
-
-    Raises:
-        FileNotFoundError: no file found at given path.
-    """
-    mk_json_outpath(out_path)
-
-    try:
-        with open(out_path, "w", encoding="UTF-8") as json_outfile:
-            json.dump(out_dict, json_outfile, ensure_ascii=False, indent=4)
-
-    except FileNotFoundError:
-        print(f"\nFile at {out_path} not found!")
-        sys.exit(1)
-
-
-def write_merged_dict_to_jsonfile(out_dict: dict, out_path: str) -> None:
-    """
-    Recursively merge dictionaries and write them to an output JSON file.
-
-    Get the desired output path, open and read any JSON data that may
-    already be there, and recursively merge in param data from the
-    most recent round of API calls.
-
-    Args:
-        out_dict (dict): dict of data from round of API calls
-            to merge and write.
-        out_path (str): path to output file.
-    """
-    # attempt to read JSON out of output file. Will return
-    # empty dict if no valid json is found
-    json_dict = read_jsonfile_into_dict(out_path)
-
-    # recursively merge all dicts and nested dicts in both dictionaries
-    dict_utils.merge_dicts_recursive(json_dict, out_dict)
-
-    # write JSON content back to file
-    write_dict_to_jsonfile(json_dict, out_path)
